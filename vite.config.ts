8e311a425c748d8df760564f47b745a691ada23e
Oliwia Bandola <oliwia.bandola@gmail.com>
2025-05-11 21:31:43 +0200

    Change href in confirmation.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Oliwia_Bandola_Web_Wroclaw",
+  base: "/Oliwia_Bandola_Web_Wroclaw/",
 });
